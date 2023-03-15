4f38056e1dec8f086e479fd0994cf04575cd6e24
Stephanie Goldstein <stephanieg0@github.com>
2023-03-15 11:01:06 -0500

    fixed item id bug

--- src/api/create-item-api.js
+++ src/api/create-item-api.js
@@ -3,10 +3,11 @@ import {items} from './items-data'
 export function createItemApi(request, params){
   const {itemId} = params
 	const {itemName, itemUrl, itemNotes} = request
-	const orderedIds = items.map(item => item.id).sort()
-
+	const orderedIds = items.map(item => item.id).sort((a,b) => a - b)
+	const lastIdIndex = orderedIds.length - 1
+	const newId = orderedIds[lastIdIndex] + 1
 	const newItem = {
-		id: orderedIds + 1,
+		id: newId,
 		name: itemName,
 		url: itemUrl,
 		notes: itemNotes
